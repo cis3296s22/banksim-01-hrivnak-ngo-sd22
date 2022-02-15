80ec04f0ecdf7917cbde374837b1e52d3cbecbdd
Syn555 <89527340+Syn555@users.noreply.github.com>
2022-02-15 00:27:00 -0500

    Task 2 & 3
    Task 2: Allowed transfer of multiple of banks (note there is still some error within the code, will have to fix some more).
    Task 3: Started refactoring tests.

--- src/edu/temple/cis/c3238/banksim/Bank.java
+++ src/edu/temple/cis/c3238/banksim/Bank.java
@@ -27,12 +27,22 @@ public class Bank {
     }
 
     public void transfer(int from, int to, int amount) {
-        if (accounts[from].withdraw(amount)) {
-            accounts[to].deposit(amount);
-        }
+        int lessAmount = Math.min( from, to );
+        int moreAmount = Math.max( from, to );
+
+        synchronized ( accounts[lessAmount] ) {
+            synchronized ( accounts[moreAmount] ) {
+                if (accounts[from].withdraw(amount)) {
+                    accounts[to].deposit(amount);
+                    System.out.printf("Account %d successfully transferred $%d to Account %d.\n", from, amount, to);
+                } 
+                else
+                    System.out.printf("Transfer of $%d from Account %d to Account %d failed.\n", amount, from, to);
         
         // Uncomment line when ready to start Task 3.
-        // if (shouldTest()) test();
+            if (shouldTest()) test();
+            }
+        }
     }
 
     public void test() {
@@ -60,4 +70,39 @@ public class Bank {
         return ++numTransactions % NTEST == 0;
     }
 
+    public void Test() {
+        new Test( accounts, initialBalance, numAccounts, Thread.currentThread() ).start();
+    }
+}
+
+class Test extends Thread{
+    private final Account[] accounts;
+    private final int initialBalance;
+    private final int numAccounts;
+    private Thread transferThread;
+
+    public Test( Account[] accounts, final int intialBalance, final int numAccounts, Thread transferThread ) {
+        this.accounts = accounts;
+        this.initialBalance = intialBalance;
+        this.numAccounts = numAccounts;
+        this.transferThread = transferThread;
+    }
+
+    @Override
+    public void run() {
+        int totalBalance = 0;
+        for (Account account : accounts) {
+            System.out.printf("%-30s %s%n",
+                    transferThread.toString(), account.toString());
+            totalBalance += account.getBalance();
+        }
+        System.out.printf("%-30s Total balance: %d\n", transferThread.toString(), totalBalance);
+        if (totalBalance != numAccounts * initialBalance) {
+            System.out.printf("%-30s Total balance changed!\n", transferThread.toString());
+            System.exit(0);
+        } 
+        else {
+            System.out.printf("%-30s Total balance unchanged.\n", transferThread.toString());
+        }
+    }
 }
